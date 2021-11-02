88ab6ef76ccf5781e48ac6f80d41274acf1f479a
Damir Temirgaliyev <temirgalievdamir03@gmail.com>
2021-11-02 19:51:57 +0600

    Added threads

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -1,32 +1,54 @@
 ﻿#include <iostream>
-#include <future>
+#include <mutex>
 #include "Blockchain.h"
 #include "Miner.h"
+#include <thread>
+
+using namespace std;
+
+mutex mu;
+
+double getTotalVolume(Blockchain& chain) {
+  double total = 0;
+  for (auto iter = chain.chain.begin(); iter != chain.chain.end(); ++iter) {
+    total = total + iter->getTransaction_data().amount;
+  }
+  return total;
+}
+
+void shared_mine(Miner& miner, TransactionData& tt, Blockchain& blockchain) {
+    lock_guard<mutex> guard(mu);
+
+    miner.mine(tt, blockchain);
+}
+
+void func(Miner& miner, TransactionData& tt, Blockchain& blockchain) {
+  for (int i = 0; i < 3; ++i) {
+    shared_mine(miner, tt, blockchain);
+  }
+}
+
 int main()
 {
-    //std::cout << "Hello World!\n";
-    Blockchain blockchain;
-    blockchain.printBlockchain();
-    time_t current;
-    TransactionData tt1( 111, "Omar", "Meiram", time(&current));
-    blockchain.addBlock(tt1);
-    blockchain.printBlockchain();
+  Blockchain blockchain;
+  blockchain.printBlockchain();
+  time_t current;
+  TransactionData tt1( 111, "Omar", "Meiram", time(&current));
+  TransactionData tt2(1000, "Yerzhan", "Damir", time(&current));
+  Miner miner1;
+  Miner miner2;
 
-    time_t current2;
-    TransactionData tt2(1000, "Erzhan", "Damir", time(&current2));
-    blockchain.addBlock(tt2);
-    Miner miner(blockchain);
-    if (blockchain.isBlockchainValid()) {
-        cout << "Rabotaet" << endl;
-    }
+  blockchain.printBlockchain();
+  thread thread1(func, ref(miner1), ref(tt1), ref(blockchain));
+  thread thread2(func, ref(miner2), ref(tt2), ref(blockchain));
 
-    miner.addWithout(tt2);
-    miner.addWithout(tt2);
-    miner.addWithout(tt2);
-    blockchain.printBlockchain();
+  thread1.join();
+  thread2.join();
+  blockchain.printBlockchain();
 
-    //double getTotalVolume(Blockchain &chain){
-    //    return;
-    //}
-    //async();
+  if (blockchain.isBlockchainValid()) {
+    cout << "Rabotaet" << endl;
+  }
+  cout << getTotalVolume(blockchain) << endl;
+
 }
